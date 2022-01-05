f3f7164f3d5d4c415a2ce9a2289ef32739269978
HANSEONGWOO <ghost5409@naver.com>
2022-01-05 15:01:49 +0900

    테스트 커밋

--- back-end/src/main/java/com/insoft/helpdesk/HelpDeskApplication.java
+++ back-end/src/main/java/com/insoft/helpdesk/HelpDeskApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelpDeskApplication {
-
-
+    
+    //테스트 주석
     public static void main(String[] args){
         SpringApplication.run(HelpDeskApplication.class, args);
     }
