79d875bdf0a0b28639560882c214a0ef8c86b39d
sujin112 <97423781+sujin112@users.noreply.github.com>
2022-01-21 11:07:40 +0900

    import ReactPaginate 제거

--- front-end/src/component/admin/GraphComponent.tsx
+++ front-end/src/component/admin/GraphComponent.tsx
@@ -3,30 +3,28 @@ import { procPostAxios } from "axios/Axios";
 import { response, Router } from "express";
 import React, { useEffect } from "react";
 import { useState } from "react";
-import ReactPaginate from "react-paginate";
 import Pagination from "./Pagination";
 
 function GraphComponent({tableClassName, tableData, arr} : any) {
 
-    const limit = 4; // 표시될 컨텐츠 수
+    const limit = 10; // 표시될 컨텐츠 수
     const [posts, setPosts] = useState([]);
     const [page, setPage] = useState(1);
     const offset = (page - 1) * limit;
 
     return (
         <div>
-            <div className="table-responsive table-bordered">
-                <table className="table table-sm text-center  border-dark" >
-                    <thead className="table-secondary border-dark ">
-                        <tr  >
+            <table className="table table-sm table-responsive table-bordered border-dark text-center" >
+                <thead className="table-secondary border-dark">
+                    <tr>
                         <th scope="col" className="col-md-1">#</th>
                         <th scope="col" className="col-md-2">코드</th>
                         <th scope="col">항목</th>
                         <th scope="col" className="col-md-2">등록자</th>
                         <th scope="col" className="col-md-2">등록일</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                    </tr>
+                </thead>
+                <tbody>
                     {tableData.slice(offset,offset+limit).map((table_data : any, index : number) => (
                         <tr key={index}>
                             <td scope="row">{index}</td>
@@ -36,15 +34,13 @@ function GraphComponent({tableClassName, tableData, arr} : any) {
                             <td>{table_data.REGIST_DT}</td>
                         </tr>
                     ))}
-                    </tbody>
-                </table>                           
-            </div>
+                </tbody>
+            </table>                           
 
             {/* 페이징처리 */}
             <div className="justify-content-center">
                <Pagination total={tableData.length} limit={limit} page={page} setPage={setPage} />
-            </div>
-            
+            </div>            
         </div>
     );
 }
