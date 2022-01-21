b852ce62343bad8dc8a89f1324812416f96550ba
sujin112 <97423781+sujin112@users.noreply.github.com>
2022-01-21 11:04:59 +0900

    paginate 변경

---
+++ front-end/src/component/admin/Pagination.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Pagination({ total, limit, page, setPage }) {
+  const numPages = Math.ceil(total / limit);
+
+  return (
+      <ul className="pagination pagination-sm justify-content-center">
+        <li>
+          <button className={"page-item page-link " + (page===1?"text-white":"")} onClick={() => setPage(page - 1)} disabled={page === 1}>&lt;</button>
+        </li>
+        {Array(numPages)
+          .fill(0)
+          .map((_, i) => (
+            <li key={i + 1}>
+              <button className={"page-item page-link "+(page===i+1?'bg-primary text-white':'')} onClick={() => {setPage(i + 1)}}>
+                {i + 1}
+              </button> 
+            </li>
+          ))}
+      <li>
+        <button className={"page-item page-link " + (page===numPages ?"text-white":"")} onClick={() => setPage(page + 1)} disabled={page === numPages}> &gt;</button>
+      </li>
+    </ul>
+  );
+}
+
+
+export default Pagination;
