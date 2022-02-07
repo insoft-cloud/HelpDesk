b822e4d2510c8ea25f12d87877ca824462f24418
insoft <insoft@in-soft.co.kr>
2022-02-07 14:27:22 +0900

    2022-02-07 13:55 repo update

--- back-end/src/main/java/com/insoft/helpdesk/application/domain/jpa/repo/service/RequestAttachmentHistoryRepo.java
+++ back-end/src/main/java/com/insoft/helpdesk/application/domain/jpa/repo/service/RequestAttachmentHistoryRepo.java
@@ -1,12 +1,6 @@
 package com.insoft.helpdesk.application.domain.jpa.repo.service;
 
-<<<<<<< HEAD
-import com.insoft.helpdesk.application.domain.jpa.entity.service.Request;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RequestAttachmentHistoryRepo extends JpaRepository<Request, String> {
-
-=======
 import com.insoft.helpdesk.application.domain.jpa.entity.service.RequestAttachmentHistory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,5 +10,5 @@ public interface RequestAttachmentHistoryRepo extends JpaRepository<RequestAttac
 
     Page<RequestAttachmentHistory> findAllBySvcReqNo(String svcReqNo, Pageable pageable);
     long countAllBySvcReqNo(String svcReqNo);
->>>>>>> e0672cfa9d8668f30701f34f60e0ed67819f70fb
+
 }
